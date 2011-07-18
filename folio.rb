35c11c2e236ac650b63287298581de9a5ee7908f
metamn <bartus.csongor@gmail.com>
2011-07-18 15:13:08 +0300

    code review and documentation started

--- folio.rb
+++ folio.rb
@@ -7,17 +7,27 @@
 #   folio inu.ro #=> /inu.ro will contain the Jekyll site generated
 #
 class Folio
+  
+  # Load the destination directory from passed arguments
+  # args - the command line arguments
+  #
+  # @dir - the destination directory
   def initialize(args)
     usage if args.empty? 
     @dir = args[0]
   end
   
-  # Displays a message how to use Folio
+  # Display a message how to use Folio
   def usage
     puts "Usage: folio url"
     puts "url - the destination url where the portfolio will be generated"
     puts "Example: folio inu.ro #=> /inu.ro will contain the Jekyll site generated"
     puts
+  end
+  
+  # Generate the Jekyll site 
+  def generate
+    
   end
 end
 
