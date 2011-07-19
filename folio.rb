6f8178c8ceced0cc77f5cfc6a2cabab65ac55e6e
metamn <bartus.csongor@gmail.com>
2011-07-19 15:38:43 +0300

    Skeleton and Dropbox files copied to output folder

--- folio.rb
+++ folio.rb
@@ -1,3 +1,6 @@
+require 'yaml'
+
+
 # Generate artist portfolio from Dropbox using Jekyll
 #
 # Usage: folio url
@@ -8,27 +11,83 @@
 #
 class Folio
   
+  attr_reader :dir, :config, :dropbox
+  
   # Load the destination directory from passed arguments
   # args - the command line arguments
   #
   # @dir - the destination directory
   def initialize(args)
-    usage if args.empty? 
+    raise ArgumentError, usage if args.empty? 
     @dir = args[0]
+    puts "Output directory: #{@dir}"
+    
+    load_config 'config.yml'
+  end
+  
+  # Load configuration from config.yml
+  def load_config(file)
+    raise SystemExit, no_config_file unless File.exists? file
+    
+    @config = YAML::load_file 'config.yml'
+    @dropbox = @config['dropbox_folder']
+    puts "Configuration loaded."
+  end
+  
+  # Generate the Jekyll site 
+  #
+  # - Create the output directory
+  # - Copy skeleton to output directory
+  def generate
+    mkdir(@dir) unless Dir.exists? @dir
+    skeleton
+    symlink
+  end
+  
+  # Create the output directory
+  def mkdir(dir)
+    puts "Creating output directory #{dir}"
+    Dir.mkdir dir
+  end
+  
+  # Copy skeleteon to output directory
+  def skeleton
+    puts "Copying skeleton ..."
+    system("cp -Rf skeleton/* #{@dir}/")
+  end
+  
+  # Symlink portfolio items from Dropbox into /images in output directory
+  def symlink
+    puts "Linking images from Dropbox ..."
+    raise ArgumentError, no_dropbox_folder unless Dir.exists?("#{@dropbox}/#{@dir}")
+    system("ln -s #{@dropbox}/#{@dir}/ #{@dir}/images") unless Dir.exists?("#{@dir}/images")
   end
   
   # Display a message how to use Folio
   def usage
     puts "Usage: folio url"
     puts "url - the destination url where the portfolio will be generated"
-    puts "Example: folio inu.ro #=> /inu.ro will contain the Jekyll site generated"
+    puts "Example: folio inu.ro #=> /inu.ro will contain the Jekyll site generated"    
     puts
+    exit
   end
   
-  # Generate the Jekyll site 
-  def generate
-    
+  # Display a message if Dropbox source folder doesn't exists
+  def no_dropbox_folder
+    puts "Cant find portfolio source files in Dropbox (#{@dropbox}/#{@dir}/)"
+    puts
+    exit
+  end
+  
+  # Display a message if config file not found
+  def no_config_file
+    puts "can't find configuration file 'config.yml'"
+    puts
+    exit
   end
 end
 
+
+#f = Folio.new ARGV
+#f.generate
 
--- test/test_folio.rb
+++ test/test_folio.rb
@@ -12,10 +12,43 @@ class TestFolio < Test::Unit::TestCase
     end
   end
   
+   
   context "Running folio with arguments" do
+  
+    def setup
+      @f = Folio.new ['demo']
+    end
+        
     should "not raise an exception" do
-      assert_nothing_raised(ArgumentError) { Folio.new 'param'}
+      assert_nothing_raised(ArgumentError) { @f }
     end 
+    
+    should "load configuration file" do
+      assert_not_nil @f.config
+    end
+    
+    should "raise exception when configuration file is missing" do
+      assert_raise(SystemExit) { @f.load_config 'alika' }
+    end
+    
+    should "create the output directory unless already exists" do
+      @f.mkdir 'demodir'
+      assert Dir.exists?('demodir')
+      Dir.rmdir('demodir')      
+    end
+    
+    should "copy skeleton to output directory" do
+      @f.mkdir @f.dir unless Dir.exists? @f.dir
+      @f.skeleton
+      assert File.exists? "#{@f.dir}/_config.yml"
+    end
+    
+    should "create a symlink in output direcory for images" do
+      @f.symlink
+      assert Dir.exists? "#{@f.dir}/images"
+    end
+    
+    
   end
 end
 
